ff1485b9d8b9e89c0311da1b22931b241e6bc3fb
David Safaryan <dssafaryan@gmail.com>
2017-05-07 01:15:23 +0300

    mobile-sections

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,44 @@ function mainNavVisibleToggle(e) {
   document.getElementById('nav').classList.toggle('main-nav--visible');
 }
 });
+
+  // script for Slider
+
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+// // Scritp for Price Section
+// $('.price-tabs__link').on('click', function(event){
+//   event.preventDefault();
+//     $(this).toggleClass('price-tabs__link--active');
+
+//       $('#price-finland').show();
+
+//   });
+
+
+// // Script for modal link
+//   $('#contact-link').on('click', function(event) {
+//     event.preventDefault();
+
+//    $('.modal').show();
+
+//     $('.modal__close').one('click', function() {
+//       $(this).closest('.modal').hide();
+
+//     $('.modal').on('click', function(event) {
+//       if($(event.target).hasClass('modal')) {
+//         $(this).closest('.modal').hide();}
+//     })
+//   });
+
+
+  // });
+
