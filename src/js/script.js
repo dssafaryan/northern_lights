d0a2efe18acaac4271f6dafe8441978c61fa8de5
David Safaryan <dssafaryan@gmail.com>
2017-05-08 00:09:22 +0300

    tablet

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,35 @@ $('.slider').slick({
   autoplaySpeed: 2000,
 });
 
+$('.review__slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 // // Scritp for Price Section
 // $('.price-tabs__link').on('click', function(event){
 //   event.preventDefault();
